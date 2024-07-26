b2d1c01366e9f73c9f28b9b63459e58c4b9dfda9
Amarnath_Github <amarshyam9199@gmail.com>
2024-07-26 22:41:38 +0530

    na

--- LinkedList/intro.cpp
+++ LinkedList/intro.cpp
@@ -29,8 +29,92 @@ int main(){
 
 */
 
-INSERT A NODE AT FRONT OF LINKED LIST
+/*INSERT A NODE AT BEGINNING:
 
 int main(){
-    if(Head )
+    Node *head;
+    head=NULL;
+    int arr[]={32,33,12,9};
+    for(int i=0;i<4;i++){
+
+        if(head == NULL){
+            head=new Node(arr[i]);
+        }else{
+            Node *temp;
+            temp=new Node(arr[i]);
+            temp->next=head;
+            head=temp;
+        }
+    } 
+
+//print the values
+
+    Node *temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+*/
+
+//insert at end
+
+int main(){
+    Node *head;
+    head=NULL;
+
+    int arr[]={2,3,5,12,33};
+
+    //way 1;
+
+    // for(int i=0;i<5;i++){
+    //     if(head==NULL){
+    //         head=new Node(arr[i]);
+    //     }else{
+    //         Node* tail=head;
+    //         while(tail->next!=NULL){
+    //             tail=tail->next;
+    //         }
+    //         Node* temp;
+    //         temp=new Node(arr[i]);
+
+    //         tail->next=temp;
+    //     }
+    // }
+
+    //way 2:
+
+    // for(int i=0;i<5;i++){
+    //     if(head==NULL){
+    //         head=new Node(arr[i]);
+    //     }else{
+    //         Node* tail=head;
+    //         while(tail->next!=NULL){
+    //             tail=tail->next;
+    //         }
+    //         tail->next=new Node(arr[i]);
+    //     }
+    // }
+
+    //way 3
+
+    Node *tail=NULL;
+
+    for(int i=0;i<5;i++){
+        if(head==NULL){
+            head=new Node(arr[i]);
+            tail=head;
+        }else{
+
+            tail->next=new Node(arr[i]);
+            tail=tail->next;
+        }
+    }
+
+    Node *temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
 }
---
+++ LinkedList/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+for(int i=0;i<5;i++){
+    //     if(head==NULL){
+    //         head=new Node(arr[i]);
+    //     }else{
+    //         Node* tail=head;
+    //         while(tail->next!=NULL){
+    //             tail=tail->next;
+    //         }
+    //         tail->next=new Node(arr[i]);
+    //     }
+    // }
