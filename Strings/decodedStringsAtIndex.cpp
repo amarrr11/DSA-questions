bbc4cf87a0c52efa80f855542f5f17400a0528e2
Amarnath_Github <amarshyam9199@gmail.com>
2024-07-30 00:35:00 +0530

    modified:   Strings/decodedStringsAtIndex.cpp

--- Strings/decodedStringsAtIndex.cpp
+++ Strings/decodedStringsAtIndex.cpp
@@ -28,12 +28,70 @@ Output: "a"
 Explanation: The decoded string is "a" repeated 8301530446056247680 times.
 The 1st letter is "a".*/
 
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    string decodeAtIndex(string s, int k) {
+        long long size = 0; 
+
+        // First pass: Calculate the length of the decoded string
+        for (char &c : s) {
+            if (isdigit(c)) { 
+                size *= (c - '0'); // Multiply size by the digit value
+            } else {
+                size += 1; // Increment size for each letter
+            }
+        }
+        
+        // Second pass: Find the k-th character
+        for (int i = s.size() - 1; i >= 0; i--) {
+            k = k % size; // Update k to be within the current segment size
+            
+            if (k == 0 && isalpha(s[i])) {
+                return string(1, s[i]); // Return the current letter if k is 0
+            }
+            
+            if (isalpha(s[i])) {
+                size -= 1; // Decrement size for each letter
+            } else {
+                size /= s[i] - '0'; // Divide size by the digit value
+            }
+        }
+        
+        return ""; 
+    }
+};
+
+int main() {
+    Solution sol; 
+    
+    
+    vector<pair<string, int>> testCases = {
+        {"leet2code3", 10},
+        {"a2b3", 5}, 
+        {"a2b3c4", 9},
+    };
+    
+    
+    for (const auto& [s, k] : testCases) {
+        cout << "Decoding string: " << s << " for k = " << k << endl;
+        string result = sol.decodeAtIndex(s, k); 
+        cout << "Result: " << result << endl; 
+    }
+    
+    return 0; 
+}
+
+
+/*#include <iostream>
 #include <string>
 using namespace std;
 
 char kThCharaterOfDecryptedString(string s, long long k) {
-    string str = "";
+
     long long i = 0;
     long long n = s.size();
     long long len = 0;
@@ -80,3 +138,4 @@ int main() {
 
     return 0;
 }
+*/
