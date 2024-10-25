b03e1ad5172624d2f6c784054bc7dbab10a2c143
Amarnath_Github <amarshyam9199@gmail.com>
2024-10-26 01:19:16 +0530

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,66 +1,33 @@
-#include<bits/stdc++.h>
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 int main() {
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        long long k;
-        cin>>n;
-        cin>>k;
-        vector<long long> v(k);
-        
-        for(int i=0;i<n;i++){
-            cin>>v[i];
-        }
-        bool flag=true;
-        for(int i=0;i<n;i++){
-            if(v[i]==k){
-                cout<<v[i]<<endl;
-                flag=false;
-                break;
-            }
-        }
-        if(!flag) continue;
-        int press=0;
-        
-        sort(v.begin(),v.end(),greater<int>());
-        for(int i:v){
-            press+=i;
-            if(press>k){
-                break;
-            }
-        }
-        if(press-1==k){
-            cout<<press<<endl;
-        }else{
-            cout<<press-1<<endl;
-        }
-        
-        // while(k){
-        //     int cnt=0;
-        //     for(int &i:v){
-        //         if(i==-1) continue;
-        //         if(i==0){
-        //             press++;
-        //             cnt++;
-        //             i=-1;
-        //             continue;
-        //         }
-        //         if(press>k){
-        //             cout<<press<<endl;
-        //             break;
-        //         }
-        //         cnt++;
-        //         i-=1;
-        //         press++;
-                
-        //     }
-        //     k-=cnt;
-        // }
-        
-    }
     
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        vector<ll> v(n);
+        for(auto &x:v){
+            cin>>x;
+        }
+        unordered_map<ll,ll> mp;
+        mp[0]=1;
+        ll sum=0;
+        ll ans=0;
+        for(ll i:v){
+            sum+=i;
+            if(sum==0){
+                ans++;
+                continue;
+            }
+            mp[sum]++;
+        }
+        cout<<ans<<endl;
+    }
 
     return 0;
 }
