f9267c29be8b8c8e1c28f68b31a1a8fb7ceebd2b
Amarnath_Github <amarshyam9199@gmail.com>
2024-11-29 18:32:33 +0530

    s

--- DP/Knapsack/1_01Knapsack.cpp
+++ DP/Knapsack/1_01Knapsack.cpp
@@ -38,7 +38,7 @@ int solve(int capacity, vector<int> &val, vector<int> &wt, int n) {
     
     if (wt[n - 1] <= capacity) {
         return dp[capacity][n] = max(val[n - 1] + solve(capacity - wt[n - 1], val, wt, n - 1),
-                                     solve(capacity, val, wt, n - 1));
+                                    solve(capacity, val, wt, n - 1));
     } else {
         return dp[capacity][n] = solve(capacity, val, wt, n - 1);
     }
